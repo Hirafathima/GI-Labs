0e6d53c67aab802c49f0f7b93ae7388769b1fee1
adarshsuresh07 <adarshsuresh@cet.ac.in>
2020-06-04 22:55:25 +0530

    pie model

--- src/Components/Barplot/Barplot.js
+++ src/Components/Barplot/Barplot.js
@@ -7,36 +7,53 @@ class Barplot extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            barplot: 0
-        }  
+            barplot: 0,
+        }
     }
+
     render() {
-        return (
-            <div className="plot">
-                <Container>
-                <h2>Barplot</h2>
-                <Row>
-                        Choose the need: &nbsp; &nbsp;
-                        <Form.Group>
-                            <Form.Control as="select" size="sm" className="mb-0" onChange={e => this.setState({ barplot: e.target.value })}>
-                                <option value={0}>Basic Need</option>
-                                <option value={1}>Standard Need</option>
-                                <option value={2}>Premium Need</option>
-                            </Form.Control>
-                        </Form.Group>
+        return (<div className="plot" >
+            <Container >
+                <h2 > Barplot </h2>
+                <Row >
+                    Choose the need: &nbsp; &nbsp;
+                        <Form.Group >
+                        <Form.Control
+                            as="select"
+                            size="sm"
+                            className="mb-0"
+                            onChange={e => this.setState({ barplot: e.target.value })}
+                        >
+                            < option value={0} > Basic Need </option>
+                            <option value={1} > Standard Need </option>
+                            <option value={2} > Premium Need </option>
+                        </Form.Control>
+                    </Form.Group>
                 </Row>
-                <Row>
-                    <Col>
-                    <br/><br/>
-                        {this.state.barplot == 0 ?
-                            <img className="barplot" src="https://fathomless-ridge-71442.herokuapp.com/barplot-basic" alt="Basic Need" />
-                            : this.state.barplot == 1 ?
-                                <img className="barplot" src="https://fathomless-ridge-71442.herokuapp.com/barplot-standard" alt="Standard Need" />
-                                : <img className="barplot" src="https://fathomless-ridge-71442.herokuapp.com/barplot-premium" alt="Premium Need" />
-                        }</Col>
+                <Row >
+                    <Col >
+                        <br /> < br />
+                        {
+                            this.state.barplot == 0 ?
+                                <img className="barplot"
+                                    src="https://fathomless-ridge-71442.herokuapp.com/barplot-basic"
+                                    alt="Basic Need"
+                                />
+                                :
+                                this.state.barplot == 1 ?
+                                    <img className="barplot"
+                                        src="https://fathomless-ridge-71442.herokuapp.com/barplot-standard"
+                                        alt="Standard Need"
+                                    />
+                                    : <img className="barplot"
+                                        src="https://fathomless-ridge-71442.herokuapp.com/barplot-premium"
+                                        alt="Premium Need"
+                                    />
+                        }
+                    </Col>
                 </Row>
-                </Container>
-            </div>
+            </Container>
+        </div>
         )
     }
 }
--- src/Components/Pie/Pie.js
+++ src/Components/Pie/Pie.js
@@ -1,48 +1,79 @@
 import React from "react";
 import { PieChart } from 'react-minimal-pie-chart';
-import { Row, Col } from  'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import './Pie.css'
 
+const style = {
+    pie: {
+        fontSize: "5px",
+        fontColor: "white",
+    }
+}
 
 class Pie extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-		}
-	}
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            hovered: null,
+            data:
+                [
+                    { title: 'One', value: 10, color: '#ff0000' },
+                    { title: 'Two', value: 15, color: '#bd0000' },
+                    { title: 'Three', value: 20, color: '#9c0303' },
+                    { title: 'Four', value: 10, color: '#6e0101' },
+                    { title: 'Five', value: 15, color: '#4a0101' },
+                    { title: 'Six', value: 20, color: '#fc8383' },
+                    { title: 'Seven', value: 20, color: '#ff1c1c' },
+                ],
+            percentage: []
+        }
+    }
     render() {
-		return (
+        return (
             <div className="pie">
-                    <Row className="show-grid">
-                        <Col xs={4} md={3}>
-                            <PieChart 
-                                data={[
-                                    { title: 'One', value: 10, color: '#E38627' },
-                                    { title: 'Two', value: 15, color: '#C13C37' },
-                                    { title: 'Three', value: 20, color: '#6A2135' },
-                                ]}
-                            />
-                        </Col>
-                        <Col xs={4} md={3}>
-                            <PieChart 
-                                data={[
-                                    { title: 'One', value: 10, color: '#E38627' },
-                                    { title: 'Two', value: 15, color: '#C13C37' },
-                                    { title: 'Three', value: 20, color: '#6A2135' },
-                                ]}
-                            />
-                        </Col>
-                        <Col xs={4} md={3}>
-                            <PieChart 
-                                data={[
-                                    { title: 'One', value: 10, color: '#E38627' },
-                                    { title: 'Two', value: 15, color: '#C13C37' },
-                                    { title: 'Three', value: 20, color: '#6A2135' },
-                                ]}
-                            />
-                        </Col>
-                    </Row>
+                <Row className="show-grid">
+                    <Col xs={4} md={3} styles={style.pie} className="flex justify-content-center">
+                        <PieChart
+                            data={this.state.data}
+                            animate="true"
+                            animationEasing="ease-out"
+                            reveal={98}
+                            lineWidth={80}
+                            label={({ dataEntry }) => `${Math.round(dataEntry.percentage)} %` }
+                            labelStyle={{
+                                ...style.pie,
+                            }}
+                            onMouseOver={(e, index) => {
+                                this.setState({ hovered: index });
+                            }}
+                            onMouseOut={() => {
+                                this.setState({ hovered: null });
+                            }}
+                        />
+                        {this.state.hovered !== null ?
+                            <small style={{ color: this.state.data[this.state.hovered].color }}>{this.state.data[this.state.hovered].title}</small>
+                            :
+                            <small>{" "}</small>}
+                    </Col>
+                    <Col xs={4} md={3}>
+                        <PieChart
+                            data={[
+                                { title: 'One', value: 10, color: '#E38627' },
+                                { title: 'Two', value: 15, color: '#C13C37' },
+                                { title: 'Three', value: 20, color: '#6A2135' },
+                            ]}
+                        />
+                    </Col>
+                    <Col xs={4} md={3}>
+                        <PieChart
+                            data={[
+                                { title: 'One', value: 10, color: '#E38627' },
+                                { title: 'Two', value: 15, color: '#C13C37' },
+                                { title: 'Three', value: 20, color: '#6A2135' },
+                            ]}
+                        />
+                    </Col>
+                </Row>
             </div>
         );
     }

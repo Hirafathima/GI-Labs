d037c25e9d1fdeb4d28c6cb36662a50c98e373fc
haseena-hassan <haseena2199@gmail.com>
2020-06-05 23:48:20 +0530

    model inference

--- src/Components/Model/Model.js
+++ src/Components/Model/Model.js
@@ -17,17 +17,13 @@ class Model extends React.Component {
                     <iframe src="https://fathomless-ridge-71442.herokuapp.com/map.html" width="700" height="800" frameborder="0" allowfullscreen="false" aria-hidden="false" tabindex="0"></iframe>
                     <div>
                         <div className="text-model">
-                            At Governance Innovation Labs, we aim to deliver good governance through civic engagement. In this model, citizens will be active co-producers of the public service rather than passive recipients of the public service. Governance Innovation Labs is an attempt to create a strong state through civic engagement.
+                            The visualisation shows the geographical mapping of needs data taken from citizens in Kerala. The three types of needs raised by majority of people are highlighted as coloured bubbles in the respective district regions in the map which on click gives the most demanded need of the people. The need-color label indication used is :
                         </div>
                         <div className="choices">
                             <span style={{ display: "flex", alignItems: "center" }}>
-                                <div className="choice red" /> &emsp; Basic Needs
-                            </span>
-                            <span style={{ display: "flex", alignItems: "center" }}>
-                                <div className="choice blue" /> &emsp; Standard Needs
-                            </span>
-                            <span style={{ display: "flex", alignItems: "center" }}>
-                                <div className="choice green" /> &emsp; Premium Needs
+                                 &emsp; Basic Needs <div className="choice red" />
+                                 &emsp; Standard Needs <div className="choice blue" />
+                                 &emsp; Premium Needs <div className="choice green" />
                             </span>
                         </div>
                     </div>

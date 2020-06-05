86e1bfffc9e42c60cdb6ab8ec53426b7659fd632
haseena-hassan <haseena2199@gmail.com>
2020-06-05 19:42:19 +0530

    some local changes

--- src/Components/Model/Model.js
+++ src/Components/Model/Model.js
@@ -16,7 +16,7 @@ class Model extends React.Component{
                 <h2>Model</h2>
                 <iframe src="https://fathomless-ridge-71442.herokuapp.com/map.html" width="600" height="450" frameborder="0" allowfullscreen="false" aria-hidden="false" tabindex="0"></iframe>
                 {/* <img src="http://127.0.0.1:5000/plot.png" alt="my plot"></img> */}
-                <p>govt department alerts to be done !!</p>
+                
            </div>
         )
     }
